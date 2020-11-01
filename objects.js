f263f31fa32b3b0b6216ab98c0c9bdfb7f332b91
Jere Bacher <bacherjere@gmail.com>
2020-11-01 17:06:32 -0300

    models

---
+++ objects.js
@@ -0,0 +1,38 @@
+class Owner {
+    name
+    lastname
+    email
+    password
+    phone
+}
+
+class Customer {
+    name
+    lastname
+    email
+    password
+    phone
+}
+
+let court = {
+    "type": [],
+    "players": Number,
+    "hourprice": Number,
+    "address": String,
+    "description": String,
+    "calendar": {
+        "monday": ["08", "09"],
+        "wednesday": ["08", "09"],
+        "friday": ["08", "09"]
+    },
+    "reservations": [
+        {
+            "clientuser": 1,
+            "date": "2020-08.."
+        },
+        {
+            "clientuser": 2,
+            "date": "2020-08.."
+        }
+    ]
+}
